3c3b6c84007cb4fb2d7b44f78ffd9b53a20a15c9
David Raynes <David.Raynes@teamaol.com>
2015-09-28 19:57:54 -0400

    Cookies and sessions, oh my!

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+require('express-debug')(app, {});
+
 app.use('/bower_components',
   express.static(__dirname + '/bower_components'));
 app.use('/public', express.static(__dirname + '/public'));
@@ -9,19 +11,49 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+
+var Sequelize = require('sequelize');
+var SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+var sequelize = new Sequelize(
+  "database",
+  "username",
+  "password", {
+    "dialect": "sqlite",
+    "storage": "./store/session.sqlite"
+  });
+
+var store = new SequelizeStore({ db: sequelize });
+store.sync();
+
+app.use(cookieParser());
+app.use(session({
+  saveUninitialized: false,
+  resave: false,
+  secret: 'I see dead people',
+  store: store
+}));
+
+app.use(require('morgan')('dev'));
+
 app.set('view engine', 'jade');
 
 app.get('/', function(req, res) {
-  //res.send('Hello world!');
   res.render('index');
 });
 
 app.get('/game', function(req, res) {
-  res.render('game', req.query);
+  var playerName = req.session.playerName;
+  res.render('game', { username: playerName });
 });
 
 app.post('/game', function(req, res) {
-  res.redirect('/game?username=' + req.body.username);
+  req.session.playerName = req.body.username;
+  req.session.save(function() {
+    res.redirect('/game');
+  });
 });
 
 var models = require('./models');
