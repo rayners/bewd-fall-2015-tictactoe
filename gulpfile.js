0b9321e3232773a63ea5812a02079f9f65ef505c
David Raynes <David.Raynes@teamaol.com>
2015-09-28 19:57:23 -0400

    Gulp!

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp'),
+  gls = require('gulp-live-server');
+
+gulp.task('myTask', function() {
+  console.log("Running my task!");
+});
+
+gulp.task('server', function() {
+    var server = gls('.');
+    server.start().then(function(result) {
+        console.log('Server exited with result:', result);
+    });
+    return gulp.watch(['index.js', 'models/*.js'], function(file) {
+      console.log(file);
+        server.start.apply(server);
+    });
+});
