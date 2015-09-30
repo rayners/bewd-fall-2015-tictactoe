781b34d29967013b555b5d688683bd3f95b0f2d2
David Raynes <David.Raynes@teamaol.com>
2015-09-30 19:45:51 -0400

    Use app.param and be specific with the model

--- routes/games.js
+++ routes/games.js
@@ -1,33 +1,36 @@
 var express = require('express');
 var app = express.Router();
 
-var models = require('../models');
+var board = require('../models').Board;
+
+app.param('game_id', function(req, res, next) {
+  board.findById(req.params.game_id).then(function(b) {
+    req.board = res.locals.board = b;
+    next();
+  });
+});
 
 app.get('/', function(req, res) {
-    models.Board.findAll().then(function(boards) {
-      if (req.session.user_id) {
-        models.User.findById(req.session.user_id).then(function(user) {
-          res.render('games', { boards: boards, user: user });
-        })
-      } else {
+    board.findAll().then(function(boards) {
         res.render('games', { boards: boards });
-      }
     });
 });
 
 app.get('/:game_id', function(req, res) {
-    models.Board.findById(req.params.game_id).then(function(board) {
-      res.render('individualGame', { board: board });
-    });
+  res.render('individualGame');
 });
 
+app.get('/:game_id/players', function(req, res) {
+  res.render('gamePlayers');
+})
+
 app.post('/', function(req, res) {
-    models.Board.create({ board: req.body.board })
+    board.create({ board: req.body.board })
         .then(function(board) {
             res.redirect('/games/' + board.id);
         })
         .catch(function(errors) {
-          models.Board.findAll().then(function(boards) {
+          board.findAll().then(function(boards) {
             res.render('games', { boards: boards, errors: errors });
           });
         });
